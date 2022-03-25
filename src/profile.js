90cfdc0104900ec4ec3d455c9dcc37be572411c0
BomiDo <bm.do.bomhada@gmail.com>
2022-03-26 02:56:13 +0900

    Profile: localStorage.getItem으로 이미지 저장 기능 추가

--- src/profile.js
+++ src/profile.js
@@ -1,20 +1,25 @@
 'use strict';
 
+// load Img
 const imgInput = document.querySelector('#imgInput');
 const imgInputLabel = document.querySelector('.img__input');
 const plusIcon = document.querySelector('.fas');
 const imgPreview = document.querySelector('.img__preview');
 
+// saved Name
 const loginForm = document.querySelector('.login__form');
 const loginThumbnail = document.querySelector('.login__thumb');
 const loginInput = document.querySelector('.login__input');
 const loginName = document.querySelector('.login__name');
 
+// keyword
 const IMGDATA_KEY = 'profile_img';
 const USERNAME_KEY = 'username';
 const HIDE_CLASSNAME = 'hide';
+const HIDDEN_CLASSNAME = 'hidden';
 
-imgInput.addEventListener('change', () => {
+// UserImg
+function onLoadImg() {
   console.log(imgInput.files);
   const reader = new FileReader();
   reader.onload = () => {
@@ -23,12 +28,24 @@ imgInput.addEventListener('change', () => {
     img.setAttribute('id', 'imgPreview');
     img.src = reader.result;
     imgPreview.appendChild(img);
-    imgInputLabel.style.visibility = 'hidden';
-    plusIcon.style.visibility = 'hidden';
+    plusIcon.style.visibility = HIDDEN_CLASSNAME;
   }
   reader.readAsDataURL(imgInput.files[0]);
-});
+}
 
+function prinImg(savedImgData) {
+  imgPreview.innerHTML = `<img src="${savedImgData}" alt="thumbnail">`;
+  plusIcon.style.visibility = HIDDEN_CLASSNAME;
+}
+const savedImgData = localStorage.getItem(IMGDATA_KEY);
+
+if (savedImgData === null) {
+  imgInput.addEventListener('change', onLoadImg);
+} else {
+  prinImg(savedImgData);
+}
+
+// UserName
 function onLoginSubmit(event) {
   event.preventDefault();
   loginInput.classList.add(HIDE_CLASSNAME);
@@ -49,4 +66,3 @@ if (savedUsername === null) {
 } else {
   printUsername(savedUsername);
 }
-
