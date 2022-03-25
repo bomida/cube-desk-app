98e68f607cf94fe3154a4918aeeec608a9d7582d
BomiDo <bm.do.bomhada@gmail.com>
2022-03-25 20:42:12 +0900

    README 추가

--- src/main.js
+++ src/main.js
@@ -3,19 +3,43 @@
 import MoveBGElement from "./bg.js";
 import DateTime from "./date-time.js";
 import Calculator from "./claculator.js";
+import Weather from "./weather";
 
+// calculator
 const numberBtns = document.querySelectorAll("[data-number]");
 const operationBtns = document.querySelectorAll("[data-operator]");
-const equalsButton = document.querySelector("[data-equal]");
-const deleteButton = document.querySelector("[data-delete]");
-const allClearButton = document.querySelector("[data-all-clear]");
-const prevOperandText = document.querySelector("[data-previous-operand]");
-const currOperandText = document.querySelector("[data-current-operand]");
+const equalsBtn = document.querySelector("[data-equal]");
+const deleteBtn = document.querySelector("[data-delete]");
+const allClearBtn = document.querySelector("[data-all-clear]");
+const prevOperandText = document.querySelector("[data-prev-operand]");
+const currOperandText = document.querySelector("[data-curr-operand]");
+
+// weather
+const locateText = document.querySelector('.locate');
+const tempText = document.querySelector('.temp');
+const weaText = document.querySelector('.wea__text');
+const weaIcon = document.querySelector('.wea__icon');
+
+let weatherIcon = {
+  '01': './img/01d.png',
+  '02': './img/02d.png',
+  '03': './img/03d.png',
+  '04': './img/04d.png',
+  '09': './img/09d.png',
+  '10': './img/10d.png',
+  '11': './img/11d.png',
+  '13': './img/13d.png',
+  '50': './img/50d.png'
+};
+new Weather();
+
 
 new MoveBGElement();
 const dateTime = new DateTime();
 const calculator = new Calculator(prevOperandText, currOperandText);
 
+
+// Calculator area
 numberBtns.forEach(button => {
   button.addEventListener("click", () => {
     calculator.appendNumber(button.innerText)
@@ -30,21 +54,23 @@ operationBtns.forEach(button => {
   })
 })
 
-equalsButton.addEventListener("click", button => {
+equalsBtn.addEventListener("click", () => {
   calculator.compute()
   calculator.updateDisplay()
 })
 
-allClearButton.addEventListener("click", button => {
+allClearBtn.addEventListener("click", () => {
   calculator.clear()
   calculator.updateDisplay()
 })
 
-deleteButton.addEventListener("click", button => {
+deleteBtn.addEventListener("click", () => {
   calculator.delete()
   calculator.updateDisplay()
 })
 
+
+// DateTime area
 dateTime.getDate();
 dateTime.getTime();
 
--- src/weather.js
+++ src/weather.js
@@ -1,44 +1,54 @@
 'use strict';
 
-const locateText = document.querySelector('.locate');
-const tempText = document.querySelector('.temp');
-const weaText = document.querySelector('.wea__text');
-const weaIcon = document.querySelector('.wea__icon');
 
-const TEMP_KEY = 'º';
-const API_KEY = '15b4ffd5b8bca7d558f2320a138ed0dc';
 
-let weatherIcon = {
-  '01': './img/01d.png',
-  '02': './img/02d.png',
-  '03': './img/03d.png',
-  '04': './img/04d.png',
-  '09': './img/09d.png',
-  '10': './img/10d.png',
-  '11': './img/11d.png',
-  '13': './img/13d.png',
-  '50': './img/50d.png'
-};
+export default class Weather {
+  constructor() {
+    this.TEMP_KEY = 'º';
+    this.API_KEY = '15b4ffd5b8bca7d558f2320a138ed0dc';
+  }
 
-function onGeoOk(position) {
-  const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      locateText.textContent = data.name;
-      tempText.innerText = Math.round(data.main.temp) + TEMP_KEY;
-      weaText.textContent = data.weather[0].main;
-      let icon = (data.weather[0].icon).substr(0, 2);
-      const img = document.createElement('img');
-      img.setAttribute('src', weatherIcon[icon])
-      weaIcon.append(img);
-    });
+  onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        locateText.textContent = data.name;
+        tempText.innerText = Math.round(data.main.temp) + TEMP_KEY;
+        weaText.textContent = data.weather[0].main;
+        let icon = (data.weather[0].icon).substr(0, 2);
+        const img = document.createElement('img');
+        img.setAttribute('src', weatherIcon[icon])
+        weaIcon.append(img);
+      });
+  }
+
+  onGeoError() {
+    alert(`Can't find you.\nNo weather for you. :(`);
+  }
 }
 
-function onGeoError() {
-  alert(`Can't find you.\nNo weather for you. :(`);
-}
+// function onGeoOk(position) {
+//   const lat = position.coords.latitude;
+//   const lng = position.coords.longitude;
+//   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//       locateText.textContent = data.name;
+//       tempText.innerText = Math.round(data.main.temp) + TEMP_KEY;
+//       weaText.textContent = data.weather[0].main;
+//       let icon = (data.weather[0].icon).substr(0, 2);
+//       const img = document.createElement('img');
+//       img.setAttribute('src', weatherIcon[icon])
+//       weaIcon.append(img);
+//     });
+// }
+
+// function onGeoError() {
+//   alert(`Can't find you.\nNo weather for you. :(`);
+// }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
